cfdf1571c9b95d317d24e2ba5bb1c6d5237c5633
Evandro Xavier <exavier@MacBook-Air-de-Evandro.local>
2024-04-14 23:31:32 -0300

    Projeto Criado

---
+++ src/test/java/com/javasas/atendimento/AtendimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javasas.atendimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtendimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
